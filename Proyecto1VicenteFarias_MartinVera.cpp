955bab922f06eb3d5afff79839704afbb7d1d0c2
VeraSupremo <martiniverinirx@gmail.com>
2025-05-03 00:09:01 -0400

    comit de prueba

--- Proyecto1VicenteFarias_MartinVera.cpp
+++ Proyecto1VicenteFarias_MartinVera.cpp
@@ -5,7 +5,8 @@
 #define LECTURAS_CONSECUTIVAS 1     // Lectores que han leído consecutivamente
 #define MAX_LECTURAS_CONSECUTIVAS 5 // Maximo de Lectores que han leído consecutivamente
 #define ESCRITORES_ESPERANDO 1   // Maximum number of readings
-#define MAX_ESPERA 5      // Maximum wait time in seconds
+#define MAX_ESPERA 5      // tiempo maximo de espera en lectores cuando se llegue al max
+#define MAX_ESPERAESCRITOR = 10 //tiempo maximo de espera de los escritores
 
 using namespace std;
 int main() {
